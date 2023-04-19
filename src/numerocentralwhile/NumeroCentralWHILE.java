17955aade9373f88f712c7afdbb2ac9c46f6c91f
jaimediazcam <1755120@alu.murciaeduca.es>
2023-04-19 11:43:19 +0200

    Modificacion añadida

--- src/numerocentralwhile/NumeroCentralWHILE.java
+++ src/numerocentralwhile/NumeroCentralWHILE.java
@@ -23,6 +23,8 @@ public class NumeroCentralWHILE {
             num1 = sc.nextInt();
             num2 = sc.nextInt();
             num3 = sc.nextInt();
+            
+            System.out.println("Hola");
 
             if (num1 > num2 && num1 < num3 || num1 < num2 && num1 > num3) {
                 System.out.println("El numero central es: " + num1);
